49453ab66242dacbc33a8e121d9119efa28ad8f1
Sriram Natarajan <sriram.rajaa@tamu.edu>
2019-11-07 23:47:55 -0600

    YEET

--- camera.py
+++ camera.py
@@ -3,6 +3,6 @@ from time import sleep
 camera = PiCamera()
 camera.start_preview()
 for i in range(5):
-    time.sleep(5)
+    sleep(5)
     camera.capture('/home/pi/Desktop/image%s.jpg' % i)
 camera.stop_preview()
