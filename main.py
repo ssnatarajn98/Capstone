967ebd92de1b53b23a3c6e24d92e59dd0071a3d1
Andrew Schwenn <aschwenn@verizon.net>
2019-11-07 22:13:28 -0600

    g

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import constants
 ''' GLOBAL VARIABLES '''
 toggleButton = gpiozero.Button(constants.TOGGLE_BUTTON)
 resetButton = gpiozero.Button(constants.RESET_BUTTON, hold_time=2)
+pot = gpiozero.MCP3008(channel=0)
 
 ''' AUXILIARY FUNCTIONS '''
 def toggle():
@@ -28,8 +29,11 @@ def button_reset():
   # TODO
   print("Device reset triggered!")
 
-''' SETUP '''
+''' SIGNAL CALLBACKS '''
 toggleButton.when_pressed = toggle
 resetButton.when_held = button_reset
+
+while True:
+  print(pot.value)
 
 pause()
