b5b269096362094f398ccecf6000f8a639bcde14
Sriram Natarajan <sriram.rajaa@tamu.edu>
2019-11-12 18:33:42 -0600

    capped number

--- cameraTest.py
+++ cameraTest.py
@@ -6,8 +6,9 @@ camera.start_preview()
 cnt =  0
 if not os.path.exists('/home/pi/Desktop/ImageTestFolder/'):
     os.makedirs('/home/pi/Desktop/ImageTestFolder/')
-while(1):
+while(100):
     cnt = cnt + 1
     sleep(.2)
     camera.capture('/home/pi/Desktop/ImageTestFolder/imageTest%s.jpg' % cnt)
+    print("printed %s photos" % cnt)
 camera.stop_preview()
