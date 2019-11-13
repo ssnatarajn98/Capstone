cce470e6d415b63fe790ce1bb73e3f4e414456a2
Sriram Natarajan <sriram.rajaa@tamu.edu>
2019-11-12 19:29:25 -0600

    5 pic

--- cameraTest.py
+++ cameraTest.py
@@ -6,7 +6,7 @@ camera.start_preview()
 cnt =  0
 if not os.path.exists('/home/pi/Desktop/ImageTestFolder/'):
     os.makedirs('/home/pi/Desktop/ImageTestFolder/')
-while(100):
+while(5):
     cnt = cnt + 1
     sleep(.2)
     camera.capture('/home/pi/Desktop/ImageTestFolder/imageTest%s.jpg' % cnt)
