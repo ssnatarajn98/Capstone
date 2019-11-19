0df54a8c6d4bd02343db25ce6146a445fc6ec0b0
Sriram Natarajan <sriram.rajaa@tamu.edu>
2019-11-19 14:56:05 -0600

    Fsd

--- lastMinuteTest.py
+++ lastMinuteTest.py
@@ -6,25 +6,25 @@ import constants
 import display
 
 lowCamera = PiCamera()
-lowCamera.resolution =  (640, 480)
-#lowCamera.resolution = (2592,1944)
+#lowCamera.resolution =  (640, 480)
+lowCamera.resolution = (2592,1944)
 #lowCamera.resolution = (1280,960)
 
 
 
 def allTestPictures():
-    if not os.path.exists('/home/pi/Desktop/OutdoorTestImages/'):
-        os.makedirs('/home/pi/Desktop/OutdoorTestImages/')
+    if not os.path.exists('/home/pi/Desktop/OutdoorTestImagesHighQuality/'):
+        os.makedirs('/home/pi/Desktop/OutdoorTestImagesHighQuality/')
 
 def setupHeightFolder(height):
     allTestPictures()
-    currDir = "/home/pi/Desktop/OutdoorTestImages/height-" + str(height)+"/"
+    currDir = "/home/pi/Desktop/OutdoorTestImagesHighQuality/height-" + str(height)+"/"
     if not os.path.exists(currDir):
         os.makedirs(currDir)
 
 def setupWidthFolder(height, width):
     setupHeightFolder(height)
-    currDir = "/home/pi/Desktop/OutdoorTestImages/height-" + str(height)+"/"+"width-"+str(width)+"/"
+    currDir = "/home/pi/Desktop/OutdoorTestImagesHighQuality/height-" + str(height)+"/"+"width-"+str(width)+"/"
     if not os.path.exists(currDir):
         os.makedirs(currDir)
 
@@ -58,7 +58,7 @@ def takePictures(height, width):
     cnt = 0
     while(cnt<3):
         cnt = cnt + 1
-        currImage = "/home/pi/Desktop/OutdoorTestImages/height-" + str(normHeight)+"/"+"width-"+str(normWidth)+"/low-"+str(cnt)+".jpg"
+        currImage = "/home/pi/Desktop/OutdoorTestImagesHighQuality/height-" + str(normHeight)+"/"+"width-"+str(normWidth)+"/high-"+str(cnt)+".jpg"
         lowCamera.capture(currImage)
     lowCamera.stop_preview()
     print("\t finished photo")
