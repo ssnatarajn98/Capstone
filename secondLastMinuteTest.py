2c1c684509424c1635f59c695d868ff155371bfd
Sriram Natarajan <sriram.rajaa@tamu.edu>
2019-11-22 14:31:11 -0600

    ds

---
+++ secondLastMinuteTest.py
@@ -0,0 +1,10 @@
+import os
+from picamera import PiCamera
+from time import sleep
+import gpiozero
+import constants
+import display
+
+toggleButton.when_pressed = toggle_cb
+resetButton.when_held = button_reset_cb
+
