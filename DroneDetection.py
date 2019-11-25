03dae4299303cbf75b73fe49165346f018092e4c
Sriram Natarajan <sriram.rajaa@tamu.edu>
2019-11-24 22:16:21 -0600

    Fsdf

--- DroneDetection.py
+++ DroneDetection.py
@@ -14,7 +14,7 @@ class dataset:
         self.data = data
 
 # Data found from analyzing spreadsheet
-constants = [dataset(40,[[30, 182], [50, 238]]),
+c = [dataset(40,[[30, 182], [50, 238]]),
              dataset(50,[[30, 117], [60, 233]]),
              dataset(60,[[30, 142], [50, 210], [70, 248]]),
              dataset(70,[[30, 120], [40, 155], [80, 239]]),
@@ -25,7 +25,7 @@ constants = [dataset(40,[[30, 182], [50, 238]]),
 # Given distance from center (pixels) and height (dm), returns width (dm)
 def getWidth(distance, height):
     def getWidth2(distance, height):
-        for i in constants:
+        for i in c:
             if i.height == height:
                 length = len(i.data)
                 if distance < i.data[0][1]:
@@ -36,11 +36,11 @@ def getWidth(distance, height):
                     if distance < i.data[j][1]:
                         return (distance - i.data[j-1][1]) / (i.data[j][1] - i.data[j-1][1]) * (i.data[j][0] - i.data[j-1][0]) + i.data[j-1][0]
                 return -1
-    for i in range(1, len(constants)):
-        if height <= constants[i].height:
-            widthLow = getWidth2(distance, constants[i-1].height)
-            widthHigh = getWidth2(distance, constants[i].height)
-            return (height - constants[i-1].height) / 10 * (widthHigh - widthLow) + widthLow
+    for i in range(1, len(c)):
+        if height <= c[i].height:
+            widthLow = getWidth2(distance, c[i-1].height)
+            widthHigh = getWidth2(distance, c[i].height)
+            return (height - c[i-1].height) / 10 * (widthHigh - widthLow) + widthLow
 #-------------
 rightEdge = {}
 leftEdge = {}
